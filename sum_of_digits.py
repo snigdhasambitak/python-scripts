c49ae4f89e0f4f143fdedc691d9f0c190388e096
Snigdha Sambit Aryakumar <snigdhasambit.aryakumar@here.com>
2021-04-23 22:33:40 +0530

    sum_of_digits

---
+++ sum_of_digits.py
@@ -0,0 +1,7 @@
+###Given a number, find the sum of its digits. Take the number as an input from the user
+
+
+def sum_of_digits():
+    return(sum([int(i) for i in input("enter the  num")]))
+
+print(sum_of_digits())12345
