d2bf3199dace0e77d4d1d2ff9deba4ff450a93f3
Snigdha Sambit Aryakumar <snigdhasambit.aryakumar@here.com>
2021-04-23 20:01:52 +0530

    spy_game

---
+++ spy_game.py
@@ -0,0 +1,20 @@
+#### SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
+
+     # spy_game([1,2,4,0,0,7,5]) --> True
+     # spy_game([1,0,2,4,0,5,7]) --> True
+     # spy_game([1,7,2,0,4,5,0]) --> False
+
+
+def spy_game(arr):
+    bond = [0,0,7,'x']
+    for item in arr:
+        if item == bond[0]:
+            bond.pop(0)
+    return len(bond) == 1
+
+
+print(spy_game([1,2,4,0,0,7,5]))
+
+print(spy_game([1,0,2,4,0,5,7]))
+
+print(spy_game([1,7,2,0,4,5,0]))
