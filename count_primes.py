68ef7aceca38acc34e9c1c5ed36a18b2902ac94b
Snigdha Sambit Aryakumar <snigdhasambit.aryakumar@here.com>
2021-04-23 22:09:53 +0530

    count_primes

---
+++ count_primes.py
@@ -0,0 +1,20 @@
+#### COUNT PRIMES: Write a function that returns the *number* of prime numbers that exist up to and including a given number
+    # count_primes(100) --> 25
+
+# By convention, 0 and 1 are not prime.
+
+
+def count_primes(num):
+    counter = 0
+    for n in range(2,num+1):
+        prime=1
+        for i in range(2,n):
+            if(n%i == 0):
+                prime += 1
+                break
+        if prime == 1:
+            counter += 1
+    return counter
+
+
+print(count_primes(100))
